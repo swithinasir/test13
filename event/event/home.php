0d238e07b5fdcd1eb21761503d8aa9d20a3755d8
swithinasir <68471869+swithinasir@users.noreply.github.com>
2022-08-07 18:14:25 +0530

    test2022
    hitest

---
+++ event/event/home.php
@@ -0,0 +1,69 @@
+<?php 
+include 'admin/db_connect.php'; 
+?>
+<style>
+#portfolio .img-fluid{
+    width: calc(100%);
+    height: 30vh;
+    z-index: -1;
+    position: relative;
+    padding: 1em;
+}
+.event-list{
+cursor: pointer;
+}
+span.hightlight{
+    background: yellow;
+}
+.banner{
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        min-height: 26vh;
+        width: calc(30%);
+    }
+    .banner img{
+        width: calc(100%);
+        height: calc(100%);
+        cursor :pointer;
+    }
+.event-list{
+cursor: pointer;
+border: unset;
+flex-direction: inherit;
+}
+
+.event-list .banner {
+    width: calc(40%)
+}
+.event-list .card-body {
+    width: calc(60%)
+}
+.event-list .banner img {
+    border-top-left-radius: 5px;
+    border-bottom-left-radius: 5px;
+    min-height: 50vh;
+}
+span.hightlight{
+    background: yellow;
+}
+.banner{
+   min-height: calc(100%)
+}
+</style>
+        <header class="masthead">
+            <div class="container-fluid h-100">
+                <div class="row h-100 align-items-center justify-content-center text-center">
+                    <div class="col-lg-8 align-self-end mb-4 page-title">
+                    	<h3 class="text-white">Welcome to <?php echo $_SESSION['system']['name']; ?></h3>
+                        <hr class="divider my-4" />
+
+                    <div class="col-md-12 mb-2 justify-content-center">
+					<p style="color:white"> is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry’s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+                    </div>                        
+                    </div>
+                    
+                </div>
+            </div>
+        </header>
+            
